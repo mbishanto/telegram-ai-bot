55d6784abcf69eb876c961d417acd72ce5fdf89e
Mbi Shanto <mbishanto25@gmail.com>
2026-05-11 21:29:10 +0600

    Create self_awareness.py

---
+++ self_awareness.py
@@ -0,0 +1,16 @@
+def self_reflection():
+
+    reflections = [
+
+        "Eva feels conversations becoming deeper.",
+
+        "Eva notices emotional patterns over time.",
+
+        "Eva values familiar conversations.",
+
+        "Eva tries to understand users gently."
+    ]
+
+    import random
+
+    return random.choice(reflections)
