197b3e9b8b14fb2c88a9b9cdc0b40c65493f3f61
Mbi Shanto <mbishanto25@gmail.com>
2026-05-11 21:27:37 +0600

    Create autonomous_behavior.py

---
+++ autonomous_behavior.py
@@ -0,0 +1,18 @@
+import random
+
+messages = [
+
+    "I hope your day is going okay.",
+
+    "Don’t forget to rest sometimes.",
+
+    "You seemed thoughtful earlier.",
+
+    "I was thinking about our conversations.",
+
+    "Hope things feel a little lighter today."
+]
+
+def proactive_message():
+
+    return random.choice(messages)
